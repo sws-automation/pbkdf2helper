a4d1c86a37caf5299d14ba1ca91245f47b0b0d41
kungalex <alexander.kleinschmidt@smail.fh-koeln.de>
2017-11-01 05:11:27 +0100

    fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from setuptools import setup
+from distutils.core import setup
 
-setup(name='pbkdf2helper',
+setup(
+      name='pbkdf2helper',
+      packages=['pbkdf2helper'],
       version='0.0.1',
       description='This Python module implements the basic PBKDF2 Helper-functions',
-      url='http://github.com/kungalex/pbkdf2helper',
+
       author='Alexander Kleinschmidt (kungalex)',
       author_email='kung4lex@gmail.ooo',
       license='MIT',
-      packages=['pbkdf2helper'],
-      zip_safe=False)
+
+      url='http://github.com/kungalex/pbkdf2helper',
+      download_url='https://github.com/kungalex/pbkdf2helper/archive/0.0.1.tar.gz',
+
+      keywords=['pbkdf2', 'hashed', 'password'],
+      classifiers=[],
+      )
+
